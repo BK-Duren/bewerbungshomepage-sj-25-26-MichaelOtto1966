5c3a797cd58cbfcaa26a8a46a42a82f50f0bae4d
m.otto <60357676+MichaelOtto1966@users.noreply.github.com>
2026-05-06 10:27:32 +0000

    Einfügen des Menüs

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
 
+/* -- ----------------------------- */
+/* -- BKSD - Remove Active          */
+/* -- ----------------------------- */
+function active_remove() {
+        $('.active').each(function(index) {
+         $(this).removeClass('active');
+       });
+    }
+/* -- ------------------------------ */
+/* -- BKSD - Set Active Menü         */
+/* -- ------------------------------ */
+ function setActiveMenu(id) {
+       /* Entferne Active-Markierung */
+       active_remove();
+       /* Füge Klasse Active hinzu */
+       jQuery(id).addClass('active');
+    }
